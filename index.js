eb8d377660b39de635e804ee88e07f599fd4ae27
ColinLi33 <Colin8tor1206@gmail.com>
2023-11-18 11:27:35 -0800

    uploaded csv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const PC = require('./pc');
 const User = require('./user')
+const fs = require("fs");
+const { parse } = require("csv-parse");
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,25 @@ app.get('/', (req, res) => {
   res.send('Welcome to my server!');
 });
 
+app.get('/signin', (req, res) => {
+    res.send('Welcome to my server!');
+});
+
+function readPCs(){
+    fs.createReadStream("./pcData.csv")
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+        console.log(row);
+    })
+    .on("end", function () {
+        console.log("finished");
+    })
+    .on("error", function (error) {
+        console.log(error.message);
+    });
+}
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    readPCs()
+    console.log(`Server is running on port ${port}`);
 }); 
